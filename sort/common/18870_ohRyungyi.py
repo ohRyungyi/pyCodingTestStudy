3a4eae9c8c3106a9b5d70349692f460da1a57361
yelim oh <66195824+ohRyungyi@users.noreply.github.com>
2021-07-29 23:02:16 +0900

    Delete 18870_ohRyungyi.py

--- sort/common/18870_ohRyungyi.py
+++
@@ -1,9 +0,0 @@
-# 만약 정확한 값이 필요 없고 값의 대소 관계만 필요하다면, 모든 수를 0 이상 N 미만의 수로 바꿀 수 있습니다.
-
-import sys
-
-N = int(sys.stdin.readline())
-
-data_list = [int(x) for x in sys.stdin.readline().split('\n')[0].split(' ')]
-
-print(data_list)
