a80b3ae0f8b6149c4bd8c143f4e96e15dd1b7a8a
ohryungyi <dodoh0125@konkuk.ac.kr>
2021-07-29 23:00:33 +0900

    7월 29일자 목요일자 완료

--- sort/common/18870_ohRyungyi.py
+++ sort/common/18870_ohRyungyi.py
@@ -1,18 +1,9 @@
+# 만약 정확한 값이 필요 없고 값의 대소 관계만 필요하다면, 모든 수를 0 이상 N 미만의 수로 바꿀 수 있습니다.
+
 import sys
 
 N = int(sys.stdin.readline())
 
-order = list()
-ind = list()
+data_list = [int(x) for x in sys.stdin.readline().split('\n')[0].split(' ')]
 
-data = list(sys.stdin.readline().split('\n')[0].split(' '))
-
-for i in data:
-    i = int(i)
-    if i not in ind:
-        ind.append(i)
-    order.append(i)
-
-ind.sort()
-for i in order:
-    print(ind.index(i), end=' ')
+print(data_list)
