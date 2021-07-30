e3901cb6557dea667e6bbfc5d6b0993d676cf1f3
ohryungyi <dodoh0125@konkuk.ac.kr>
2021-07-30 21:20:35 +0900

    7월 30일자 금요일 프로그래머스 가장 큰수 완료

--- sort/common/가장큰수_ohRyungyi.py
+++ sort/common/가장큰수_ohRyungyi.py
@@ -1,24 +1,16 @@
+
 def solution(numbers):
     answer = ''
-    num_dict = dict()
-    for num in numbers:
-        ind = int(str(num)[0])
-        if ind not in num_dict.keys():
-            num_dict[ind] = list()
-            num_dict[ind].append(num)
-        else :
-            num_dict[ind].append(num)
+    num_list = list()
 
-    num_list = sorted(num_dict.items(), key = lambda x: x[0], reverse = True)
-    for num in num_list:
-        nums = num[1]
-        # nums_list = sorted(nums , key = lambda x : x[1:], reverse = True)
-        print(nums)
-        # for n in nums:
-        #     answer+=str(n)
-        for n in  nums:
+    num_list = sorted(list(map(str, numbers)), key = lambda x : str(x)*5, reverse= True)
+
+    answer = str(int(''.join(num_list)))
             
     return answer
 
-# print(solution([6, 10, 2]))
+print(solution([6, 10, 2]))
+print()
 print(solution([3, 30, 34, 5, 9]))
+print()
+print(solution([121, 12]))
