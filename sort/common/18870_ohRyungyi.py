f8869a80e8129f63e51862b66ef31cde16bc2fca
ohryungyi <dodoh0125@konkuk.ac.kr>
2021-08-01 23:19:45 +0900

    8월 1일자 코드 업로드

---
+++ sort/common/18870_ohRyungyi.py
@@ -0,0 +1,23 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+int_list = [int(sys.stdin.readlin().split())]
+
+print(int_list)
+
+'''
+입력
+5
+2 4 -10 4 -9
+
+출력
+2 3 0 3 1
+
+입력
+6
+1000 999 1000 999 1000 999
+
+출력
+1 0 1 0 1 0
+'''
