ef132b5eb86a632393af155c4b06ee89b4569559
ohryungyi <dodoh0125@konkuk.ac.kr>
2021-08-03 22:58:48 +0900

    8월 3일 화요일 백준 체스판 다시 칠하기 문제 완료

--- exhaustive_search/baekJoon/1018_체스판다시칠하기/ohRyungyi.py
+++ exhaustive_search/baekJoon/1018_체스판다시칠하기/ohRyungyi.py
@@ -7,12 +7,9 @@ M = int(M)
 boards = []
 
 for i in range(N):
-    # boards.append([])
-    # print()
     data = list(map(str, sys.stdin.readline().split('\n')[0]))
-    # print(data)
     boards.append(data)
-    # boards[i].append(sys.stdin.readline().split('\n')[0])
+    
 
 type1 = list()
 type1.append(list(map(str, 'WB'*4)))
@@ -23,10 +20,7 @@ type1.append(list(map(str, 'WB'*4)))
 type1.append(list(map(str, 'BW'*4)))
 type1.append(list(map(str, 'WB'*4)))
 type1.append(list(map(str, 'BW'*4)))
-# type1 = 'WB'*4+"\n"+'BW'*4+'\n'+'WB'*4+"\n"+'BW'*4+'\n'+'WB'*4+"\n"+'BW'*4+'\n'+'WB'*4+"\n"+'BW'*4
-# print('\n'+type1)
-# type2 = 'BW'*4+'\n'+'WB'*4+'\n'+'BW'*4+'\n'+'WB'*4+'\n'+'BW'*4+'\n'+'WB'*4+'\n'+'BW'*4+'\n'+'WB'*4
-# print('\n'+type2)
+
 type2 = list()
 type2.append(list(map(str, 'BW'*4)))
 type2.append(list(map(str, 'WB'*4)))
@@ -64,42 +58,6 @@ for i in range(N):
             if counts > num2:
                 counts = num2
 print(counts)
-
-# def repaint(boards, x, y, N, M, counts) :
-#     numw = 0
-#     numb = 0
-    
-#     if (N)-x <8 and (M)-y<8:
-#         # print(x, y, counts)
-#         return counts
-#     elif (M)-y<8:
-#         # print(x, y, counts)
-#         return repaint(boards, x+1, 0, N, M, counts)
-#     elif (N)-x <8:
-#         # print(x, y, counts)
-#         return repaint(boards, x, y+1, N, M, counts)
-#     else:
-#         for i in range(x, x+8):
-#             for j in range(y, y+8):
-#                 if type1[i-x][j-y] != boards[i][j]:
-#                     numw+=1
-#                 if type2[i-x][j-y] != boards[i][j]:
-#                     numb+=1
-#         if counts > numw:
-#             counts = numw
-#         if counts > numb:
-#             counts = numb
-#         # print(x, y, counts, numw, numb)
-#         return repaint(boards, x, y+1, N, M, counts)
-
-# print(repaint(boards, 0, 0, N, M, N*M))
-
-# for i in range(0, N-7):
-#     for j in range(0, M-7):
-#         print(boards[i][j:j+8])
-
-# print(boards)
-
 
 '''
 입력
