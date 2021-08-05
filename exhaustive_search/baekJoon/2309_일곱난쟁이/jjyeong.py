63ee1392ba41a0d4540ff7654ef7a013dac5dbe7
yesjiyoung <rohjy77@gmail.com>
2021-08-05 21:36:57 +0900

    8월 5일 목요일 백준 일곱 난쟁이 문제 풀이 추가

--- exhaustive_search/baekJoon/2309_일곱난쟁이/jjyeong.py
+++ exhaustive_search/baekJoon/2309_일곱난쟁이/jjyeong.py
@@ -1,16 +1,29 @@
+# 첫번째풀이 ## sorted(li)<-★제일빨랐음 / li.sort()
 import sys
 
 li = [int(sys.stdin.readline()) for _ in range(9)]
-#print(li)
-
-#print(sum(li))
 from itertools import combinations
-
-#print(list(combinations(li,2)))
 for com in combinations(li,2):
     if sum(li) - sum(com) == 100:
         li.remove(com[0])
         li.remove(com[1])
         break
-
 print(*sorted(li), sep='\n')
+
+
+# # 두번째풀이 : 7개로 조합하면 ?
+# import sys
+
+# li = [int(sys.stdin.readline()) for _ in range(9)]
+# print(li)
+
+# print(sum(li))
+# from itertools import combinations
+
+# print(list(combinations(li,2)))
+# for com in combinations(li,7):
+#     if sum(com) == 100:
+#         break
+# com = list(com)
+# com.sort()
+# print(*com, sep='\n')
