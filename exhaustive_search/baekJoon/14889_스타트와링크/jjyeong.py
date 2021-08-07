c22b39260ae94a6de44599995e6a71bfd5433d01
yesjiyoung <rohjy77@gmail.com>
2021-08-07 23:44:20 +0900

    8월 7일 토요일 백준 스타트와 링크 문제 완료

---
+++ exhaustive_search/baekJoon/14889_스타트와링크/jjyeong.py
@@ -0,0 +1,44 @@
+import sys 
+from itertools import permutations, combinations
+
+N = int(sys.stdin.readline()) 
+team_num = list(range(N))
+score_set = []
+for _ in range(N):
+    score_set.append(list(map(int, sys.stdin.readline().split())))
+
+# 먼저 팀 조합을 구해야함. 
+min_deduct_val = 999999999
+for one_combi in combinations(team_num, int(N/2)):
+    
+    team_copy = team_num.copy()
+    for c in one_combi:
+        team_copy.remove(c)
+    the_other_combi = team_copy
+
+    one_team_val = 0
+    the_other_team_val = 0
+    #print(f"combi : {one_combi}")
+    for one_permu, the_other_permu in zip(permutations(one_combi,2),permutations(the_other_combi,2)): #(0,1) (1,0)
+        #print(f"one_permu : {one_permu}")
+        #print(f"the_other_permu : {the_other_permu}")
+        one_team_val += score_set[one_permu[0]][one_permu[1]]
+        the_other_team_val += score_set[the_other_permu[0]][the_other_permu[1]]
+        #print(f"one_team_val : {one_team_val}")
+        #print(f"one_team_val : {the_other_team_val}")
+
+
+    #print(f"one_team_val : {one_team_val}")
+
+    deduct_val = abs(the_other_team_val - one_team_val)
+    #print(f"deduct_val : {deduct_val}")
+    
+    if deduct_val == 0:
+        min_deduct_val = deduct_val
+        #print("0이다")
+        break 
+
+    min_deduct_val = min(deduct_val, min_deduct_val)
+    #print(f"min_deduct_val : {min_deduct_val}")
+
+print(min_deduct_val)
