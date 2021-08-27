554b11bc91d5fba790ec2ee8b97c7251a7bacf59
park-daeun <70877497+park-daeun@users.noreply.github.com>
2021-08-27 21:28:59 +0900

    Create park-daeun.py

---
+++ KAKAO/2021 BLIND/신규 아이디 추천/park-daeun.py
@@ -0,0 +1,85 @@
+def solution(new_id):
+    answer = ''
+
+    # 1
+    answer = new_id.lower()
+    #print(answer)
+
+    # 2
+    check = answer[:]
+    for s in check:
+        if s.isalnum() == False and s != '-' and s != '_' and s != '.':
+            answer = answer.replace(s, '')
+    #print(answer)
+    
+    # 3
+    while answer.count('..') > 0:
+        answer = answer.replace('..', '.')    
+    #print(answer)
+    
+    # 4
+    if len(answer) >= 1 and answer[0] == '.':
+        answer = answer[1:]
+    if len(answer) >= 1 and answer[-1] == '.':
+        answer = answer[:-1]
+    #print(answer)
+
+    # 5
+    if len(answer) == 0:
+        answer = 'a'
+    #print(answer)
+    
+    # 6
+    if len(answer) >= 16:
+        answer = answer[:15]
+        while answer[-1] == '.':
+            answer = answer[:-1]
+    #print(answer)
+
+    # 7 
+    if len(answer) <= 2:
+        while len(answer) < 3:
+            answer += answer[-1]
+    #print(answer)
+
+    return answer
+
+new_id = "...!@BaT#*..y.abcdefghijklm"
+print(solution(new_id))
+
+new_id = "z-+.^."
+print(solution(new_id))
+
+new_id = "=.="
+print(solution(new_id))
+
+new_id = "123_.def"
+print(solution(new_id))
+
+new_id = "abcdefghijklmn.p"
+print(solution(new_id))
+
+'''
+1단계 new_id의 모든 대문자를 대응되는 소문자로 치환합니다.
+2단계 new_id에서 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자를 제거합니다.
+3단계 new_id에서 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환합니다.
+4단계 new_id에서 마침표(.)가 처음이나 끝에 위치한다면 제거합니다.
+5단계 new_id가 빈 문자열이라면, new_id에 "a"를 대입합니다.
+6단계 new_id의 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거합니다.
+     만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.
+7단계 new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
+
+입력 예시
+new_id = "...!@BaT#*..y.abcdefghijklm"
+new_id = "z-+.^."
+new_id = "=.="
+new_id = "123_.def"
+new_id = "abcdefghijklmn.p"
+
+출력 예시
+"bat.y.abcdefghi"
+"z--"
+"aaa"
+"123_.def"
+"abcdefghijklmn"
+'''
